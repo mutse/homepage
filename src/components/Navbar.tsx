1e5ff2bc3b071fd5555dfd3d765b14d5987be658
mutse <yyhoo2.young@gmail.com>
2024-05-12 21:33:11 +0800

    update navbar items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,31 @@ export default function Navbar() {
                 href="https://chatgpt.mutse.top"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                AI                
+                ChatGPT                
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://bing.mutse.top"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Bing
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://claude.mutse.top"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Claude
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://ai.mutse.top"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                AI
               </a>
             </li>
             <li>
